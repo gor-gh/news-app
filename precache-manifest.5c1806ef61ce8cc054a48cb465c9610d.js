c1e83e643cbcd296f1de1a14fcb97cee2d67bc15
Gor Gharagyozyan <gorkaragyozyan@gmail.com>
2020-07-10 18:42:54 +0400

    Initial app commit

---
+++ precache-manifest.5c1806ef61ce8cc054a48cb465c9610d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482477891cb92474019906d90d5b06f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b314b9566518137830f",
+    "url": "/static/css/main.893a456a.chunk.css"
+  },
+  {
+    "revision": "8a04ab49e1cd4c18dcb7",
+    "url": "/static/js/2.b54f3577.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b54f3577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b314b9566518137830f",
+    "url": "/static/js/main.fc50059e.chunk.js"
+  },
+  {
+    "revision": "5b529171394eb05713fd",
+    "url": "/static/js/runtime-main.dfb4f9aa.js"
+  }
+]);
